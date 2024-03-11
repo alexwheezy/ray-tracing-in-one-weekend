a0bd2d07430a9d7921877086a4680cc8e899a419
Alexander Alekhin <sphrand@gmail.com>
2024-03-11 09:03:16 +0300

    Implemented the Hittable trait for the Sphere.

--- src/hittable.rs
+++ src/hittable.rs
@@ -1,13 +1,31 @@
-use crate::{ray::Ray, vec3::Point3, vec3::Vec3};
+use crate::{
+    ray::Ray,
+    vec3::{self, Point3, Vec3},
+};
 
-trait Hittable {
-    fn hit(&self, r: &Ray, t_min: f32, t_max: f32, rec: &HitRecord) -> bool;
+pub trait Hittable {
+    fn hit(&self, r: &Ray, t_min: f32, t_max: f32, rec: &mut HitRecord) -> bool;
 }
 
+#[derive(Default)]
 pub struct HitRecord {
     p: Point3,
     normal: Vec3,
     t: f32,
+    front_face: bool,
+}
+
+impl HitRecord {
+    pub fn set_face_normal(&mut self, r: &Ray, outward_normal: Vec3) {
+        // Sets the hit record normal vector.
+        // NOTE: the parameter `outward_normal` is assumed to have unit length.
+        self.front_face = vec3::dot(r.direction(), &outward_normal) < 0.0;
+        self.normal = if self.front_face {
+            outward_normal
+        } else {
+            -outward_normal
+        };
+    }
 }
 
 pub struct Sphere {
@@ -22,7 +40,34 @@ impl Sphere {
 }
 
 impl Hittable for Sphere {
-    fn hit(&self, r: &Ray, t_min: f32, t_max: f32, rec: &HitRecord) -> bool {
-        todo!()
+    fn hit(&self, r: &Ray, t_min: f32, t_max: f32, rec: &mut HitRecord) -> bool {
+        let oc = *r.origin() - self.center;
+        let a = r.direction().length_squared();
+        let half_b = vec3::dot(&oc, r.direction());
+        let c = oc.length_squared() - (self.radius * self.radius);
+        let discriminant = (half_b * half_b) - a * c;
+
+        if discriminant < 0.0 {
+            return false;
+        }
+
+        let sqrtd = discriminant.sqrt();
+
+        // Find the nearest root that lies in the acceptable range.
+        let root = (-half_b - sqrtd) / a;
+        if root < t_min || t_max < root {
+            let root = (-half_b + sqrtd) / a;
+            if root < t_min || t_max < root {
+                return false;
+            }
+        }
+
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        rec.normal = (rec.p - self.center) / self.radius;
+        let outward_normal = (rec.p - self.center) / self.radius;
+        rec.set_face_normal(r, outward_normal);
+
+        true
     }
 }
