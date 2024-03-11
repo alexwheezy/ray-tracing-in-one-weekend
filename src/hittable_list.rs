4063b66740315e970657c3271fc079e3b4032791
Alexander Alekhin <sphrand@gmail.com>
2024-03-11 09:08:08 +0300

    Storage for storing Hittable type objects.

---
+++ src/hittable_list.rs
@@ -0,0 +1,44 @@
+use crate::hittable::{HitRecord, Hittable};
+use crate::ray::Ray;
+
+use std::rc::Rc;
+
+#[derive(Default)]
+pub struct HittableList {
+    objects: Vec<Rc<dyn Hittable>>,
+}
+
+impl HittableList {
+    pub fn new() -> Self {
+        Self { objects: vec![] }
+    }
+
+    pub fn with_object(&mut self, object: Rc<dyn Hittable>) {
+        self.add(object);
+    }
+
+    pub fn add(&mut self, object: Rc<dyn Hittable>) {
+        self.objects.push(object);
+    }
+
+    pub fn clear(&mut self) {
+        self.objects.clear();
+    }
+}
+
+impl Hittable for HittableList {
+    fn hit(&self, r: &Ray, t_min: f32, t_max: f32, rec: &mut HitRecord) -> bool {
+        let mut temp_rec = HitRecord::default();
+        let mut hit_anything = false;
+        let mut closest_so_far = t_max;
+
+        for object in &self.objects {
+            if object.hit(r, t_min, closest_so_far, &mut temp_rec) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                *rec = temp_rec;
+            }
+        }
+        hit_anything
+    }
+}
