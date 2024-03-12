1eb81c919e12a0cadd68c025ff196bdbd0eb09ed
Alexander Alekhin <sphrand@gmail.com>
2024-03-12 11:56:08 +0300

    Adding new ones to render into the scene.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use ray_tracing_in_one_weekend::{camera, hittable, hittable_list::HittableList, vec3::Point3};
+use ray_tracing_in_one_weekend::{
+    camera, color, hittable, hittable_list::HittableList, material, vec3::Point3,
+};
 
 use std::rc::Rc;
 
@@ -7,13 +9,35 @@ fn main() {
 
     // World
     let mut world = HittableList::default();
-    world.add(Rc::new(hittable::Sphere::new(
-        Point3::new(0.0, 0.0, -1.0),
-        0.5,
-    )));
+
+    // Materials
+    let material_ground = Rc::new(material::Lambertian::new(color::Color::new(0.8, 0.8, 0.0)));
+    let material_center = Rc::new(material::Lambertian::new(color::Color::new(0.7, 0.3, 0.3)));
+    let material_left = Rc::new(material::Metal::new(color::Color::new(0.8, 0.8, 0.8)));
+    let material_right = Rc::new(material::Metal::new(color::Color::new(0.8, 0.6, 0.2)));
+
     world.add(Rc::new(hittable::Sphere::new(
         Point3::new(0.0, -100.5, -1.0),
         100.0,
+        Some(material_ground),
+    )));
+
+    world.add(Rc::new(hittable::Sphere::new(
+        Point3::new(0.0, 0.0, -1.0),
+        0.5,
+        Some(material_center),
+    )));
+
+    world.add(Rc::new(hittable::Sphere::new(
+        Point3::new(-1.0, 0.0, -1.0),
+        0.5,
+        Some(material_left),
+    )));
+
+    world.add(Rc::new(hittable::Sphere::new(
+        Point3::new(1.0, 0.0, -1.0),
+        0.5,
+        Some(material_right),
     )));
 
     // Image
