4fadb0d8280c740d89481425caca6cb5d9cb2b05
Alexander Alekhin <sphrand@gmail.com>
2024-03-14 11:07:31 +0300

    Refactoring code.

--- src/color.rs
+++ src/color.rs
@@ -1,8 +1,9 @@
 use crate::{interval::Interval, vec3::Vec3};
+use std::io::Write;
 
 pub type Color = Vec3;
-pub fn write_color(pixel_color: Color, samples_per_pixel: u32) {
-    let (mut r, mut g, mut b) = (pixel_color.x(), pixel_color.y(), pixel_color.z());
+pub fn write_color(f: &mut impl Write, pixel_color: Color, samples_per_pixel: u32) {
+    let (mut r, mut g, mut b) = (pixel_color[0], pixel_color[1], pixel_color[2]);
 
     // Divide the color by the number of samples.
     let scale = 1.0 / samples_per_pixel as f32;
@@ -16,12 +17,14 @@ pub fn write_color(pixel_color: Color, samples_per_pixel: u32) {
 
     // Write the translated [0,255] value of each color component.
     let intensity = Interval::new(0.0, 0.999);
-    println!(
+    writeln!(
+        f,
         "{} {} {}",
         (256.0 * intensity.clamp(r)) as i32,
         (256.0 * intensity.clamp(g)) as i32,
         (256.0 * intensity.clamp(b)) as i32
-    );
+    )
+    .expect("Writing color failed");
 }
 
 #[inline]
