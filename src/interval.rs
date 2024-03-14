52982a99eafd55c0dc1deb1796b4887357a39d97
Alexander Alekhin <sphrand@gmail.com>
2024-03-14 11:09:25 +0300

    Fixed a bug in the type constructor.

--- src/interval.rs
+++ src/interval.rs
@@ -1,4 +1,4 @@
-use crate::rtweekend::INFINITY;
+use std::f32::INFINITY;
 
 pub struct Interval {
     pub min: f32,
@@ -8,8 +8,8 @@ pub struct Interval {
 impl Default for Interval {
     fn default() -> Self {
         Self {
-            min: INFINITY,
-            max: -INFINITY,
+            min: -INFINITY,
+            max: INFINITY,
         }
     }
 }
@@ -42,5 +42,54 @@ impl Interval {
     #[inline]
     pub fn universe() -> Self {
         Self::new(-INFINITY, INFINITY)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_contains() {
+        let interval = Interval::new(1.0, 2.0);
+        assert!(interval.contains(1.0));
+
+        assert!(interval.contains(1.5));
+        assert!(interval.contains(2.0));
+
+        assert!(!interval.contains(0.0));
+        assert!(!interval.contains(3.0));
+    }
+
+    #[test]
+    fn test_surrounds() {
+        let interval = Interval::new(1.0, 2.0);
+        assert!(!interval.surrounds(1.0));
+
+        let interval = Interval::new(1.5, 2.0);
+        assert!(!interval.surrounds(1.0));
+    }
+
+    #[test]
+    fn test_clamp() {
+        let interval = Interval::new(1.0, 2.0);
+        assert_eq!(interval.clamp(0.5), 1.0);
+        assert_eq!(interval.clamp(1.5), 1.5);
+        assert_eq!(interval.clamp(0.0), 1.0);
+
+        assert_eq!(interval.clamp(3.0), 2.0);
+        assert_eq!(interval.clamp(-0.5), 1.0);
+    }
+
+    #[test]
+    fn test_smoke() {
+        assert_eq!(Interval::default().min, -INFINITY);
+        assert_eq!(Interval::default().max, INFINITY);
+
+        assert_eq!(Interval::universe().min, -INFINITY);
+        assert_eq!(Interval::universe().max, INFINITY);
+
+        assert_eq!(Interval::empty().min, INFINITY);
+        assert_eq!(Interval::empty().max, -INFINITY);
     }
 }
