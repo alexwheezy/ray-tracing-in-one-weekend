40b182c498386a9cc4269ec07cca18e2ed727b63
Alexander Alekhin <sphrand@gmail.com>
2024-03-28 19:12:35 +0300

    Refactoring code.

--- src/bin/sansara.rs
+++ src/bin/sansara.rs
@@ -1,96 +1,10 @@
-use ray_tracing_in_one_weekend::{
-    camera, color, hittable,
-    hittable_list::HittableList,
-    material::{Dielectric, Lambertian, MaterialType, Metal},
-    render, rtweekend,
-    vec3::Point3,
-    vec3::Vec3,
-};
-
-use std::sync::Arc;
-
-fn generate_scene() -> HittableList {
-    let mut world = HittableList::new();
-
-    let ground_material =
-        MaterialType::Lambertian(Lambertian::new(color::Color::new(0.5, 0.5, 0.5)));
-    world.add(Arc::new(hittable::Sphere::new(
-        Point3::new(0.0, -1000.0, 0.0),
-        1000.0,
-        ground_material,
-    )));
-
-    for a in -20..20 {
-        for b in -20..20 {
-            let choose_mat = rtweekend::random_double();
-            let center = Point3::new(
-                a as f32 + 0.9 * rtweekend::random_double(),
-                0.2,
-                b as f32 + 0.9 * rtweekend::random_double(),
-            );
-
-            if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9 {
-                if choose_mat < 0.8 {
-                    // Diffuse
-                    let albedo = Vec3::random_vector() * Vec3::random_vector();
-                    let sphere_material = MaterialType::Lambertian(Lambertian::new(albedo));
-                    world.add(Arc::new(hittable::Sphere::new(
-                        center,
-                        0.2,
-                        sphere_material,
-                    )));
-                } else if choose_mat < 0.9 {
-                    // Metal
-                    let albedo = Vec3::random_vector_range(0.5, 1.0);
-                    let fuzz = rtweekend::random_double_range(0.0, 0.5);
-                    let sphere_material = MaterialType::Metal(Metal::new(albedo, fuzz));
-                    world.add(Arc::new(hittable::Sphere::new(
-                        center,
-                        0.2,
-                        sphere_material,
-                    )));
-                } else {
-                    // Glass
-                    let sphere_material = MaterialType::Dielectric(Dielectric::new(1.5));
-                    world.add(Arc::new(hittable::Sphere::new(
-                        center,
-                        0.2,
-                        sphere_material,
-                    )));
-                }
-            }
-        }
-    }
-
-    let material1 = MaterialType::Dielectric(Dielectric::new(1.5));
-    world.add(Arc::new(hittable::Sphere::new(
-        Point3::new(0.0, 1.0, 0.0),
-        1.0,
-        material1,
-    )));
-
-    let material2 = MaterialType::Lambertian(Lambertian::new(color::Color::new(0.4, 0.2, 0.1)));
-    world.add(Arc::new(hittable::Sphere::new(
-        Point3::new(-4.0, 1.0, 0.0),
-        1.0,
-        material2,
-    )));
-
-    let material3 = MaterialType::Metal(Metal::new(color::Color::new(0.7, 0.6, 0.5), 0.0));
-    world.add(Arc::new(hittable::Sphere::new(
-        Point3::new(4.0, 1.0, 0.0),
-        1.0,
-        material3,
-    )));
-
-    world
-}
+use ray_tracing_in_one_weekend::{camera, generate_scene, render, vec3::Point3};
 
 fn main() {
     env_logger::init();
 
     // World
-    let world = generate_scene();
+    let world = generate_scene::random_sphere();
 
     // Image
     // Ratio of image width over height
